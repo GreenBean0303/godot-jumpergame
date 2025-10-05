faf9b0f5b5e22f5302097a6b013ee26e7b7d50fa
GreenBean0303 <lootsmannagnes@gmail.com>
2025-10-05 18:50:39 +0300

    more

---
+++ scripts/globals/facts.gd
@@ -0,0 +1,7 @@
+extends Node
+
+var voco_facts : Array = [
+	"Kosmeetiku eriala oli aastal 2025 kõige populaarsem",
+	"VOCO on Eesti suurim kutsekool",
+	"Vähemalt pool õppeajast moodstub praktiline õpe"
+]
