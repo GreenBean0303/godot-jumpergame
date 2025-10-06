8b8b6e257f25c5fa503a3a005549b6bf061842ec
GreenBean0303 <lootsmannagnes@gmail.com>
2025-10-06 18:32:23 +0300

    Parandasin paar viga

--- scripts/globals/facts.gd
+++ scripts/globals/facts.gd
@@ -1,7 +1,17 @@
 extends Node
-
 var voco_facts : Array = [
-	"Kosmeetiku eriala oli aastal 2025 kõige populaarsem",
-	"VOCO on Eesti suurim kutsekool",
-	"Vähemalt pool õppeajast moodstub praktiline õpe"
+	"Kosmeetiku eriala oli 2025. aastal üks populaarsemaid erialasid",
+	"VOCO on Eesti suurim kutseõppeasutus",
+	"Vähemalt pool õppeajast moodustab praktiline õpe",
+	"2024. aastal õppis VOCOs üle 3600 õppija tasemeõppes",
+	"VOCOs saab omandada üle 80 erinevat eriala",
+	"VOCO pakub rahvusvahelist praktikat Erasmus+ programmi raames",
+	"VOCOs on seitse teemakooli: ilukool, ärikool, tehnikakool, ehituskool, IT-kool, toidukool ja turismikool",
+	"VOCO asutati 1922. aastal ja tegutseb juba üle 100 aasta",
+	"Paljud lõpetajad jätkavad õpinguid rakenduskõrgkoolis või ülikoolis",
+	"VOCOs on kaks kaasaegset õpilaskodu",
+	"VOCO Tartu õppehooned asuvad Kopli 1c ja Põllu 11 aadressidel",
+	"VOCO pakub rahvusvahelisi vahetusprogramme Erasmus+ kaudu",
+	"Täienduskoolituses osaleb igal aastal tuhandeid täiskasvanuid",
+	"VOCO õpilased saavutavad igal aastal edukalt tulemusi kutsevõistlustel"
 ]
